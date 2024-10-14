1f50ad87ffa8a3ac95f85f8bb40b663ee3cf00f4
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2024-10-14 18:52:19 +0100

    if/else

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+a<script>
+  export default {
+    data() {
+      return {
+        name: "Elena Sparshott",
+        status: "pending",
+        tasks: ["Task One", "Task Two", "Task Three" ],
+        link: 'https://google.com'
+      };
+    },
+    methods: {
+      toogleStatus() {
+        if (this.status === "active ") {
+
+        }
+      }
+    }
+
+  }
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status">User is inactive</p>
+  <a v-bind:href="link">Click here for Google</a>
+  <a :href="link">Click for Google</a>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li> 
+  </ul>
+</template>
+
+
